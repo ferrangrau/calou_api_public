98880a6345f5fc3fdaba8f68b24b2a8cc264f7d5
Ferran Grau Horta <ferran.grau@scmspain.com>
2019-07-30 22:44:49 +0200

    the problem is the graph, try with multiproces

--- _calou_api/run.py
+++ _calou_api/run.py
@@ -1,16 +1,70 @@
 import flask
 import os
 import ftplib
-import gc
-
 import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use('Agg')
 
 from flask import request
 from flask import jsonify
 from datetime import datetime
 
+import multiprocessing as mp
+
 
 app = flask.Flask(__name__)
+
+
+def worker(voltage):
+    date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+    filename = "data_{}.txt".format(datetime.now().strftime("%m"))
+    # if last modification date of the file was one year before, empty file
+    if os.path.isfile(filename):
+        stat = os.stat(filename)
+
+        if datetime.fromtimestamp(stat.st_mtime).strftime("%m") < datetime.now().strftime("%m"):
+            open(filename, 'w').close()
+
+    num = float(voltage)
+    if num < 11.6:
+        charge = "0%"
+    elif num < 12.0:
+        charge = "30%"
+    elif num < 12.2:
+        charge = "50%"
+    elif num < 12.5:
+        charge = "75%"
+    else:
+        charge = "100%"
+
+    with open(filename, "a") as myfile:
+        myfile.write("{}, {}\n".format(date, voltage))
+
+    # Data for plotting
+    t = []
+    s = []
+    with open(filename) as fp:
+        for cnt, line in enumerate(fp):
+            data = line.split(",")
+            t.append(cnt)
+            s.append(float(data[1].strip()))
+
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel="{} - {} - {}V".format(date, charge, voltage), ylabel='voltage (V)')
+    ax.grid()
+
+    fig.savefig("battery.png")
+
+    # upload to FTP
+    session = ftplib.FTP(os.environ['URL'], os.environ['USERNAME'], os.environ['PASSWORD'])
+    file = open('battery.png', 'rb')
+    session.storbinary('STOR battery.png', file)
+    file.close()
+    session.quit()
 
 
 @app.route('/', methods=['GET'])
@@ -25,58 +79,14 @@ def api_id():
         result['result'] = 'ok'
         voltage = float(request.args['v'])
 
-        date = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        proc = mp.Process(target=worker, args=(voltage,))
+        proc.daemon = True
+        proc.start()
+        proc.join()
 
-        filename = "data_{}.txt".format(datetime.now().strftime("%m"))
-        # if last modification date of the file was one year before, empty file
-        if os.path.isfile(filename):
-            stat = os.stat(filename)
-
-            if datetime.fromtimestamp(stat.st_mtime).strftime("%m") < datetime.now().strftime("%m"):
-                open(filename, 'w').close()
-
-        num = float(voltage)
-        if num < 11.6:
-            charge = "0%"
-        elif num < 12.0:
-            charge = "30%"
-        elif num < 12.2:
-            charge = "50%"
-        elif num < 12.5:
-            charge = "75%"
-        else:
-            charge = "100%"
-
-        with open(filename, "a") as myfile:
-            myfile.write("{}, {}\n".format(date, voltage))
-
-        # Data for plotting
-        t = []
-        s = []
-        with open(filename) as fp:
-            for cnt, line in enumerate(fp):
-                data = line.split(",")
-                t.append(cnt)
-                s.append(float(data[1].strip()))
-
-        fig, ax = plt.subplots()
-        ax.plot(t, s)
-
-        ax.set(xlabel="{} - {} - {}V".format(date, charge, voltage), ylabel='voltage (V)')
-        ax.grid()
-
-        fig.savefig("battery.png")
-
-        # upload to FTP
-        session = ftplib.FTP(os.environ['URL'], os.environ['USERNAME'], os.environ['PASSWORD'])
-        file = open('battery.png', 'rb')
-        session.storbinary('STOR battery.png', file)
-        file.close()
-        session.quit()
     else:
         result['result'] = 'ko'
 
-    gc.collect()
     return jsonify(result)
 
 
