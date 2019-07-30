c2d2c4603ff326309e196b35a36a0db671864dfd
Ferran Grau Horta <ferran.grau@scmspain.com>
2019-07-30 10:50:49 +0200

    add mapper

--- _calou_api/run.py
+++ _calou_api/run.py
@@ -79,12 +79,6 @@ def api_id():
         all_objects = muppy.get_objects()
         sum1 = summary.summarize(all_objects)  # Prints out a summary of the large objects
         summary.print_(sum1)  # Get references to certain types of objects such as dataframe
-        dataframes = [ao for ao in all_objects if isinstance(ao, pd.DataFrame)]
-        for d in dataframes:
-            print
-            d.columns.values
-            print
-            len(d)
 
     else:
         result['result'] = 'ko'
